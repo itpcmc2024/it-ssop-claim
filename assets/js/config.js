60bdda1d20f2d2eff8787c5d1622d040792930d3
itpcmc2024 <itpcmc2024@gmail.com>
2026-07-24 14:47:30 +0700

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,6 +1,6 @@
 /* SSOP Toolkit V2.3.1 Configuration */
 window.SSOP_CONFIG = {
-  apiUrl: 'PASTE_YOUR_APPS_SCRIPT_WEB_APP_URL_HERE',
+  apiUrl: 'https://script.google.com/macros/s/AKfycbx7us4idH-tM3QZXbxAilyf-ujh4glTO34Q_RhjgIskBWG3sFah7Az1PXbB1BJj62nQRg/exec',
   version: '2.3',
   defaultModule: 'SSOCAC'
 };
