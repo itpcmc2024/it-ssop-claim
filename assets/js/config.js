52336482e62dbec4c763a47ae4b360dd5c265358
itpcmc2024 <itpcmc2024@gmail.com>
2026-07-24 16:29:34 +0700

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,4 +1,4 @@
-/* SSOP Toolkit V2.3.3 Configuration */
+/* SSOP Toolkit V2.3.4 Configuration */
 window.SSOP_CONFIG = {
   apiUrl: 'https://script.google.com/macros/s/AKfycbx7us4idH-tM3QZXbxAilyf-ujh4glTO34Q_RhjgIskBWG3sFah7Az1PXbB1BJj62nQRg/exec',
   version: '2.3',
