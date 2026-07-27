2b04a6e14490d2869f346150a76debc3778675e2
itpcmc2024 <itpcmc2024@gmail.com>
2026-07-27 09:58:03 +0700

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,5 +1,5 @@
 window.SSOP_CONFIG = Object.freeze({
-  apiUrl: 'PUT_YOUR_APPS_SCRIPT_WEB_APP_URL_HERE',
+  apiUrl: 'https://script.google.com/macros/s/AKfycbwAbRE_axXsC60c43DF0YkPhM6gEV1zG4QFCyGBcD1ZFT4AN0XBomryHHnsdk6DYa7jag/exec',
   googleClientId: 'PUT_YOUR_GOOGLE_CLIENT_ID_HERE.apps.googleusercontent.com',
   version: '2.4-phase1-gis'
 });
