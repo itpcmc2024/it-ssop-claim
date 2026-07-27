cef9f1a49bc65d508b1f5077c8e53186e96f029f
itpcmc2024 <itpcmc2024@gmail.com>
2026-07-27 11:49:59 +0700

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,6 +1,6 @@
 window.SSOP_CONFIG = Object.freeze({
   apiUrl: 'https://script.google.com/macros/s/AKfycbztQYOpzMGJ16oCWviNFuEOqMgC-wMWb0cmtusxGAA9E62SzczHp93TJxaqoXVrS_N2/exec',
-  version: '2.4.2-ssocac-cute',
+  version: '2.4.4-ssocac-ready',
   mockUser: {
     email: 'itpcmc2024@gmail.com',
     displayName: 'Panisara',
