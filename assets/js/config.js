148e2e68a54d56113da227ea744a8f025ba82b72
itpcmc2024 <itpcmc2024@gmail.com>
2026-07-27 10:14:45 +0700

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,5 +1,11 @@
 window.SSOP_CONFIG = Object.freeze({
-  apiUrl: 'https://script.google.com/macros/s/AKfycbwAbRE_axXsC60c43DF0YkPhM6gEV1zG4QFCyGBcD1ZFT4AN0XBomryHHnsdk6DYa7jag/exec',
-  googleClientId: 'PUT_YOUR_GOOGLE_CLIENT_ID_HERE.apps.googleusercontent.com',
-  version: '2.4-phase1-gis'
+  version: '2.4-phase1-ui',
+  mockMode: true,
+  defaultPage: 'dashboard',
+  user: {
+    email: 'itpcmc2024@gmail.com',
+    displayName: 'Panisara',
+    role: 'ADMIN',
+    department: 'งานสารสนเทศ'
+  }
 });
