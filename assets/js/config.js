91b16bc760544c9e9447e189082d9e7a19ffa355
itpcmc2024 <itpcmc2024@gmail.com>
2026-07-29 21:09:02 +0700

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,8 +1,9 @@
-/* SSOP Claim V3.0.0 Configuration */
+/* SSOP Claim V3.0.1 Configuration */
 window.SSOP_CONFIG = {
   apiUrl: 'https://script.google.com/macros/s/AKfycbyqUFg3dFgW8US6_jekM4BLsFGARVbsLVFJIgIpv4edDuYskzei4tpCuLv6wz09Gl79Rw/exec',
-  version: '3.0.0',
+  version: '3.0.1',
   environment: 'TEST',
   repository: 'it-ssop-claim',
   defaultModule: 'SSOCAC'
 };
+
