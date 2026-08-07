0f0f972668fd6cedc9fc8d6ccfd87d5702965fda
itpcmc2024 <itpcmc2024@gmail.com>
2026-08-07 10:32:03 +0700

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,7 +1,7 @@
-/* SSOP Toolkit V4.4.1 Production Configuration */
+/* SSOP Toolkit V4.4.2 Production Configuration */
 window.SSOP_CONFIG = {
   apiUrl: 'https://script.google.com/macros/s/AKfycbxI_G_yQMCii-NsSp4-hX3I-dTF0_0hNE0ktghhEHSyIDUKpJZO9oGS8tokY5Hu0IeP4g/exec',
-  version: '4.4.1',
+  version: '4.4.2',
   environment: 'PRODUCTION',
   repository: 'it-ssop-claim',
   defaultModule: 'SSOCAC'
